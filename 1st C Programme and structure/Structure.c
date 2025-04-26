f4a5a916fc6f4807ed97e0e7f8daf1950ea56249
Snu Seam <siamahme766@gmail.com>
2025-04-27 00:22:24 +0600

    first commit

---
+++ 1st C Programme and structure/Structure.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// This is main function
+int main()
+
+{
+    // if we want declare a variable
+    int a=10,b=5,c=15;
+    //printf also a function where it tells show it in display
+    printf("This is my first code in C \n");
+    printf("Sum of 12 and 13 is : 25 \n");
+    printf("sum of %d and %d is : %d \n", 12,13,25);
+    printf("The sum of %d and %d = %d", a,b,c);
+}
