6d6859b27280e4781c53bec2bf910297b8de1fb2
Snu Seam <siamahme766@gmail.com>
2025-04-30 10:12:56 +0600

    commit

---
+++ 1st C Programme and structure/Convert To Asccii and binary/convert.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    char ch= 'A';
+    printf("%d \n",ch);
+    int a=65;
+    printf("%c \n",a);
+    printf("Enter a Value");
+    int num;
+    printf("Enter a number to convert to");
+    scanf("%d",&num);
+    printf("%c",num);
+}
